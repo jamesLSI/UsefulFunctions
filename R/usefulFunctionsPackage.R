4c6d7e47d783b79c39251e639ce57aa8c8f81703
jamesLSI <james.currer@innovateuk.ukri.org>
2023-01-05 16:42:57 +0000

    adding in function definiotns and data

--- R/usefulFunctionsPackage.R
+++ R/usefulFunctionsPackage.R
@@ -3,15 +3,20 @@ options(java.parameters = c("-XX:+UseConcMarkSweepGC", "-Xmx8192m"))
 
 ### names function ####
 
-namesFunction <- function(nms) janitor::make_clean_names(nms, case = "upper_camel")
+#' Name Repair Function
+#'
+#' @param nms used only in the .name_repair argument of readxl::read_excel and without parentheses
+#'
+#' @return variable names in dataframe in upper camel
+#' @export
+#'
+#' @examples output <- readxl::read_excel("C:/data.xlsx",
+#'                              .name_repair = namesFunction)
+namesFunction <- function(nms) {
+  janitor::make_clean_names(nms, case = "upper_camel")}
 
-### root folder ####
-rootFolder <- "~/"
 
 ### data warehouse ####
-
-
-# rootFolder <- "C:/Users/jcur01/OneDrive - UKRI/R/"
 
 #' Data Warehouse Function
 #'
@@ -23,10 +28,7 @@ rootFolder <- "~/"
 #' @examples dataWarehouse_function_specific("Application Summaries")
 dataWarehouse_function_specific <- function(table_choice = "Tables"){
 
-  # source("C:/Users/jcur01/OneDrive - UKRI/R/dwCreds.R",
-  #        chdir = T)
-
-  con <- DBI::dbConnect(drv      = RMySQL::MySQL(),
+    con <- DBI::dbConnect(drv      = RMySQL::MySQL(),
                         username = Sys.getenv("usernameDw"),
                         password = Sys.getenv("passwordDw"),
                         host     = "dw-prod.cluster-csfwpi01op01.eu-west-2.rds.amazonaws.com",
@@ -35,22 +37,20 @@ dataWarehouse_function_specific <- function(table_choice = "Tables"){
 
   all_tables <- DBI::dbListTables(con)
 
-  tables <- tibble(name = c("Applications Summaries",
-                            "Application Scores",
-                            "Assessor Comments",
-                            "Application Questions",
-                            "Projects",
-                            "Claims / Forecast",
-                            "Monitoring",
-                            "Competitions",
-                            "Change Requests",
-                            "Trans Data",
-                            "Funders",
-                            "EDI",
-                            "Warehouse",
-                            "Check Tables"))
-
-
+  tables <- tibble::tibble(name = c("Applications Summaries",
+                                    "Application Scores",
+                                    "Assessor Comments",
+                                    "Application Questions",
+                                    "Projects",
+                                    "Claims / Forecast",
+                                    "Monitoring",
+                                    "Competitions",
+                                    "Change Requests",
+                                    "Trans Data",
+                                    "Funders",
+                                    "EDI",
+                                    "Warehouse",
+                                    "Check Tables"))
   start <- Sys.time()
 
   if (table_choice == "Applications Summaries") {
@@ -237,9 +237,6 @@ dataWarehouse_function_specific <- function(table_choice = "Tables"){
     print("This could take up to a minute")
 
     output <- DBI::dbReadTable(con, "vw_Sensitive_EDIData_JC")
-    question_id <- read_csv("C:/Users/jcur01/OneDrive - UKRI/R/dataWarehouse/edi_questions_id.csv")
-    output <- output %>%
-      left_join(question_id)
 
     edi_data <<- output
 
@@ -265,20 +262,6 @@ dataWarehouse_function_specific <- function(table_choice = "Tables"){
 
 }
 
-### definitions ####
-smes <- c("Micro",
-          "Small",
-          "Medium")
-
-core <- c("Collaborative R&D",
-          "CR&D Bilateral",
-          "Feasibility Studies",
-          "GRD Development of Prototype",
-          "GRD Proof of Concept",
-          "GRD Proof of Market",
-          "Knowledge Transfer Partnership",
-          "Study")
-
 ### trans data function ####
 
 #' Transparency Data Function
@@ -288,9 +271,9 @@ core <- c("Collaborative R&D",
 #'
 #' @examples transDataFunction()
 transDataFunction <- function() {
-  link <- read_html("https://www.ukri.org/publications/innovate-uk-funded-projects-since-2004/") %>%
-    html_nodes("a") %>%
-    html_attr("href") %>%
+  link <- rvest::read_html("https://www.ukri.org/publications/innovate-uk-funded-projects-since-2004/") %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_attr("href") %>%
     str_extract('^http.+lsx$') %>%
     .[which(!is.na(.))]
 
@@ -358,7 +341,7 @@ transDataFunction <- function() {
 #'
 #' @param crn_list is a tibble of companies house reference numbers
 #'
-#' @return a tibble containing information from the companies houe API 
+#' @return a tibble containing information from the companies house API 
 #' @export
 #'
 #' @examples 
@@ -366,7 +349,7 @@ transDataFunction <- function() {
 #'     distinct(crn)
 #' companies_house_function(crn_list = crn_list)
 companies_house_function <- function(crn_list){
-  apiKey <- "V5ZW_NIGIu0ZAgw2n7YX28JFhBrN_oljEafaDSBX"
+
   if(exists("co_ho_api_output") == T){
     rm(co_ho_api_output)
   }
@@ -384,7 +367,7 @@ companies_house_function <- function(crn_list){
   for (i in 1:nrow(crn_list)) {
     try({
       apiReturn <- GET(url = paste0("https://api.companieshouse.gov.uk/company/", crn_list[i,1]),
-                       authenticate(user = apiKey, password = ""))
+                       authenticate(user = Sys.getenv("co_ho_api_key"), password = ""))
       apiJson <- suppressMessages(jsonlite::fromJSON(content(apiReturn, "text")))
       apiJson <- as.data.frame(unlist(apiJson)) %>%
         rename(value = "unlist(apiJson)")
@@ -415,20 +398,31 @@ companies_house_function <- function(crn_list){
   co_ho_api_output <- co_ho_api_output %>%
     mutate(Sic2007 = sic_codes1)
 
-  co_ho_api_output <<- co_ho_api_output
+  co_ho_company_output <<- co_ho_api_output
 }
 
 ### officer code function ####
 
-officer_code_function <- function(crn_list){
-  apiKey <- "V5ZW_NIGIu0ZAgw2n7YX28JFhBrN_oljEafaDSBX"
+#' Companies House Officer Data
+#'
+#' @param crn_list is a tibble of companies house reference numbers
+#'
+#' @return a tibble containing information from the companies house officers API
+#' @export
+#'
+#' @examples
+#' crn_list <- data %>%
+#'     distinct(crn)
+#' co_ho_officers_function(crn_list = crn_list)
+co_ho_officers_function <- function(crn_list){
+
   if(exists("api_output_officer") == T){
     rm(co_ho_api_output)
   }
   for (i in 1:nrow(crn_list)) {
     try({
       apiReturn <- GET(url = paste0("https://api.companieshouse.gov.uk/company/", crn_list[i,1], "/officers"),
-                       authenticate(user = apiKey, password = ""))
+                       authenticate(user = Sys.getenv("co_ho_api_key"), password = ""))
       apiJson <- suppressMessages(jsonlite::fromJSON(content(apiReturn, "text")))
       apiJson <- as.data.frame(unlist(apiJson)) %>%
         rename(value = "unlist(apiJson)")
@@ -452,20 +446,31 @@ officer_code_function <- function(crn_list){
   }
   return(api_output_officer)
 
-  api_output_officer <<- api_output_officer
+  co_ho_output_officer <<- api_output_officer
 }
 
 ### significant control function ####
 
-signif_control_code_function <- function(crn_list){
-  apiKey <- "V5ZW_NIGIu0ZAgw2n7YX28JFhBrN_oljEafaDSBX"
-  if(exists("api_output_officer") == T){
-    rm(co_ho_api_output)
+#' Companies House Significant Control Data
+#'
+#' @param crn_list is a tibble of companies house reference numbers
+#'
+#' @return a tibble containing information from the companies house significant control API
+#' @export
+#'
+#' @examples
+#' crn_list <- data %>%
+#'     distinct(crn)
+#' co_ho_signif_control_function(crn_list = crn_list)
+co_ho_signif_control_function <- function(crn_list){
+
+  if(exists("signif_control_output") == T){
+    rm(signif_control_output)
   }
   for (i in 1:nrow(crn_list)) {
     try({
       apiReturn <- GET(url = paste0("https://api.companieshouse.gov.uk/company/", crn_list[i,1], "/persons-with-significant-control"),
-                       authenticate(user = apiKey, password = ""))
+                       authenticate(user = Sys.getenv("co_ho_api_key"), password = ""))
       apiJson <- suppressMessages(jsonlite::fromJSON(content(apiReturn, "text")))
       apiJson <- as.data.frame(unlist(apiJson)) %>%
         rename(value = "unlist(apiJson)")
@@ -487,11 +492,87 @@ signif_control_code_function <- function(crn_list){
     rm(apiJson,
        apiReturn)
   }
-  signif_control_output <<- api_output_officer
+  co_ho_signif_control_output <<- signif_control_output
 }
 
+### charges function ####
+
+#' Companies House Charges Data
+#'
+#' @param crn_list is a tibble of companies house reference numbers
+#'
+#' @return a tibble containing information from the companies house charges API
+#' @export
+#'
+#' @examples
+#' crn_list <- data %>%
+#'     distinct(crn)
+#' co_ho_charges_function(crn_list = crn_list)
+co_ho_charges_function <- function(crn_list){
+
+  if(exists("charge_api_output") == T){
+    rm(charge_api_output)
+  }
+  
+  crn_list <- crn_list %>% 
+    rename(company_number = 1) %>% 
+    rowwise() %>% 
+    mutate(missing_zeros = 8-nchar(company_number)) %>% 
+    mutate(add_on = strrep("0",
+                           missing_zeros)) %>% 
+    mutate(Company_number_clean = paste0(add_on,
+                                         company_number)) %>% 
+    select(Company_number_clean)
+  
+  for (i in 1:nrow(crn_list)) {
+    try({
+      apiReturn <- GET(url = paste0("https://api.companieshouse.gov.uk/company/", crn_list[i,1], "/charges"),
+                       authenticate(user = Sys.getenv("co_ho_api_key"), password = ""))
+      apiJson <- suppressMessages(jsonlite::fromJSON(content(apiReturn, "text")))
+      apiJson <- as.data.frame(unlist(apiJson)) %>% 
+        rename(value = "unlist(apiJson)")
+      apiJson <- jsonlite::flatten(apiJson) %>%
+        rownames_to_column() %>% 
+        mutate(id = 1) %>% 
+        # mutate(rowname = if_else(rowname == "sic_codes",
+        #                          "sic_codes1",
+        #                          rowname)) %>% 
+        pivot_wider(names_from = rowname,
+                    values_from = value) %>% 
+        mutate(company_number = crn_list$Company_number_clean[i])
+      if(exists("charge_api_output") == F){
+        charge_api_output <- apiJson
+        
+      } else {
+        charge_api_output <- bind_rows(charge_api_output,
+                                      apiJson)
+      }
+      if (i %% 600 == 0) {
+        Sys.sleep(301)
+      } else if (i %% 100 == 0) {
+        print(i)
+      }
+    })
+    rm(apiJson,
+       apiReturn)
+  }
+  # co_ho_api_output <- co_ho_api_output %>% 
+  #   mutate(Sic2007 = sic_codes1)
+  co_ho_charges_output <<- charge_api_output
+}
 ### post code function ####
 
+#' Additional geo data from postcodes
+#'
+#' @param post_code_list is a tibble postcodes
+#'
+#' @return a tibble containing additional geographic data from the postcodes.io API
+#' @export
+#'
+#' @examples
+#' post_code_list <- data %>%
+#'     distinct(postcode)
+#' postcode_function(post_code_list = post_code_list)
 postcode_function <- function(post_code_list){
 
   for (j in 1:nrow(post_code_list)) {
@@ -537,20 +618,37 @@ postcode_function <- function(post_code_list){
 }
 
 
-### qualtrics function ####
+### qualtrics survey id function ####
+
+#' Qualtrics Survey IDs
+#'
+#' @return a tibble of survey ids and associated names
+#' @export
+#'
+#' @examples
+#' survey_id_function()
 survey_id_function <- function(){
 
-  surveys <- all_surveys() %>%
+  surveys <- qualtRics::all_surveys() %>%
     filter(!str_detect(name,
                        "DO NOT USE")) %>%
     arrange(name)
 
-  surveys <<- surveys
+  survey_ids <<- surveys
 }
 
+### qualtrics survey responses function ####
+
+#' Qualtrics Survey Responses
+#'
+#' @return a tibble of survey responses
+#' @export
+#'
+#' @examples
+#' survey_response_function()
 survey_response_function <- function(){
 
-    surveys <- all_surveys() %>%
+    surveys <- qualtRics::all_surveys() %>%
     filter(!str_detect(name,
                        "DO NOT USE")) %>%
     arrange(name)
@@ -561,18 +659,27 @@ survey_response_function <- function(){
 
   print(pcf)
 
-  mysurvey <- fetch_survey(surveyID = pcf,
+  survey_output <- qualtRics::fetch_survey(surveyID = pcf,
                            force_request = TRUE,
                            import_id = F,
                            convert = F)
-  survey_output <<- mysurvey
+  survey_responses <<- survey_output
 
 
 }
 
+### qualtrics survey questions function ####
+
+#' Qualtrics Survey Questions
+#'
+#' @return a tibble of survey questions
+#' @export
+#'
+#' @examples
+#' survey_response_function()
 survey_questions_function <- function(){
 
-  surveys <- all_surveys() %>%
+  surveys <- qualtRics::all_surveys() %>%
     filter(!str_detect(name,
                        "DO NOT USE")) %>%
     arrange(name)
@@ -581,26 +688,9 @@ survey_questions_function <- function(){
 
   pcf <- surveys$id[choice]
 
-  mysurvey <- survey_questions(surveyID = pcf)
+  survey_questions_output <- qualtRics::survey_questions(surveyID = pcf)
 
-  survey_questions <<- mysurvey
+  survey_questions <<- survey_questions_output
 
 
 }
-
-# ukri_data_hub_con <- DBI::dbConnect(odbc::odbc(),
-#                  Driver = "ODBC Driver 17 for SQL Server",
-#                  Server = "ausorizmidb01.fbf0f5dae340.database.windows.net",
-#                  Authentication = "ActiveDirectoryIntegrated")
-
-### colour palette ####
-primaryPurple <- "#4C1354"
-quantumBlue <- "#003255"
-environGreen <- "#00623E"
-grapheneGrey <- "#3C465F"
-codeRed <- "#9E0833"
-ultraViolet <- "#BD4E97"
-skyBlue <- "#4da0d9"
-luminousGreen <- "#2A926D"
-compositeGrey <- "#9699a5"
-infraRed <- "#DF5054"
