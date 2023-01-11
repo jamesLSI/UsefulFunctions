d13444a035300c36d652523086d68217d7071a9a
jamesLSI <james.currer@innovateuk.ukri.org>
2023-01-11 14:26:54 +0000

    correcting speling error

--- R/crnCleanse.R
+++ R/crnCleanse.R
@@ -12,7 +12,7 @@
 #' clean_crn_function(crn_list)
 #' 
 #' or atomic vector 
-#' clean_crn_trans_data <- clean_crn_trans_data %>% 
+#' clean_crn_trans_data <- trans_data %>% 
 #'     mutate(clean_crn = clean_crn_function(Crn)) 
 #' 
 clean_crn_function <- function(crn_list) {
@@ -23,15 +23,15 @@ clean_crn_function <- function(crn_list) {
     cleansed <- crn_list %>%
       rename(crn = 1) %>% 
       # 6 digits
-      mutate(crn_cleansed = str_replace(crn, "^(\\d{6})$", "00\\1")) %>%
+      mutate(clean_crn = str_replace(crn, "^(\\d{6})$", "00\\1")) %>%
       # 7 digits
-      mutate(crn_cleansed = str_replace(crn, "^(\\d{7})$", "0\\1")) %>%
+      mutate(clean_crn = str_replace(crn, "^(\\d{7})$", "0\\1")) %>%
       # IP withour trailing R
-      mutate(crn_cleansed = str_replace(crn, "^IP(\\d{5})$", "IP\\1R")) %>%
+      mutate(clean_crn = str_replace(crn, "^IP(\\d{5})$", "IP\\1R")) %>%
       # two letter four digits
-      mutate(crn_cleansed = str_replace(crn, "^([A-Z]{2})(\\d{4})$", "\\100\\2")) %>%
+      mutate(clean_crn = str_replace(crn, "^([A-Z]{2})(\\d{4})$", "\\100\\2")) %>%
       # two letters five digits
-      mutate(crn_cleansed = str_replace(crn, "^([A-Z]{2})(\\d{5})$", "\\10\\2"))
+      mutate(clean_crn = str_replace(crn, "^([A-Z]{2})(\\d{5})$", "\\10\\2"))
     
     clean_crn_list <<- cleansed
     
