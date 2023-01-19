b56185e9484e265244b686fe17087f956647e0ab
jamesLSI <james.currer@innovateuk.ukri.org>
2023-01-19 15:50:37 +0000

    adding data descriptions

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' SME definitions
+#'
+#' SME definitions useful for grouping organisations together
+#'
+#' @format ## `smes`
+#' A character vector with 3 entries
+"smes"
+
+#' Old Core Definitions
+#'
+#' Old Core Definitions useful for grouping IUK Product Types together
+#'
+#' @format ## `core`
+#' A character vector
+"core"
+
+#' Product Grouping Definitions
+#'
+#' New Product Grouping useful for grouping IUK Product Types together
+#'
+#' @format ## `product_groups`
+#' A character vector
+"product_groups"
