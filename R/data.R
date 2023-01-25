2893630c58ba749a8b8cb52656fd4f28fefe9a90
jamesLSI <james.currer@iuk.ukri.org>
2023-01-25 15:14:34 +0000

    adding bus pop estimates

--- R/data.R
+++ R/data.R
@@ -21,3 +21,19 @@
 #' @format ## `product_groups`
 #' A character vector
 "product_groups"
+
+#' Business Population Estimates 2010 - 2022
+#'
+#' Tidy data version of regional business population estimates for each year 2010 - 2022
+#'
+#' @format ## `bus_pop_long`
+#' A data frame
+"bus_pop_long"
+
+#' Business Population Average 2010 - 2022
+#'
+#' Tidy data version of regional business population average from 2010 - 2022
+#'
+#' @format ## `bus_pop_average`
+#' A data frame
+"bus_pop_average"
