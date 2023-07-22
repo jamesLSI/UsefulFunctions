5b961b361fd39369ba37c0f0d0f1cbbf7e6fc160
jamesLSI <james.currer@iuk.ukri.org>
2023-07-22 15:14:44 +0100

    adding lm summary table function

--- R/usefulFunctionsPackage.R
+++ R/usefulFunctionsPackage.R
@@ -770,3 +770,88 @@ co_ho_appointments_function <- function (officer_list)
   return(api_output_officer)
   co_ho_output_officer_appoints <<- api_output_officer
 }
+
+
+
+### lm summary dataframe function ####
+
+#' lm() summary dataframe function
+#'
+#' @return a tibble of releavtn output from the summary function for a linear model
+#' @export
+#'
+#' @examples
+#' Requires a lm model
+#' summary_table <- summary(linear_model) %>% 
+#'  lm_summary_table_function()
+lm_summary_table_function <- function(summary_table){
+  
+  co_effs <- summary_table$coefficients %>% 
+    as_tibble(.name_repair = namesFunction) %>% 
+    mutate(Estimate = round(Estimate,
+                            10),
+           StdError = round(StdError,
+                            10))
+  
+  if (str_detect(as.character(summary_table$call$formula)[[3]],
+                 "\\+")==T) {
+    
+    model_variable_names <- as.character(summary_table$call$formula) %>% 
+      tibble() %>%
+      rename(variable = 1) %>% 
+      filter(!variable %in% c("~",
+                              "values")) %>% 
+      mutate(variable = str_replace_all(variable,
+                                        "\\+",
+                                        "&")) %>%
+      separate_wider_delim(variable,
+                           names = c("f","s","t","fo","fi","si","se","ei","ni","te"),
+                           delim = "&",
+                           too_few = "align_start") %>% 
+      pivot_longer(1:ncol(.)) %>% 
+      filter(!is.na(value)) %>% 
+      select(variable = value,
+             -name) %>% 
+      mutate(variable = str_squish(variable))
+    
+    variable_names <- tibble(variable = "intercept") %>% 
+      bind_rows(model_variable_names)
+    
+  } else {
+    
+    variable_names <- tibble(variable = "intercept") %>% 
+      bind_rows(as.character(summary_table$call$formula) %>% 
+                  tibble() %>%
+                  rename(variable = 1) %>% 
+                  filter(!variable %in% c("~",
+                                          "values")))
+    
+  }
+  
+  output <- variable_names %>% 
+    bind_cols(co_effs) %>% 
+    mutate(signif_level = if_else(PrT < 0.001,
+                                  "***",
+                                  if_else(PrT < 0.01,
+                                          "**",
+                                          if_else(PrT < 0.05,
+                                                  "*",
+                                                  if_else(PrT < 0.1,
+                                                          ".",
+                                                          " "))))) %>% 
+    mutate(Estimate = paste(Estimate,
+                            signif_level,
+                            sep = " ")) %>% 
+    mutate(StdError = paste0("(",StdError,")")) %>% 
+    select(-c(TValue,
+              PrT,
+              signif_level)) %>% 
+    pivot_longer(2:3) %>% 
+    select(-name)
+  
+  
+  return(output)
+  
+}
+
+
