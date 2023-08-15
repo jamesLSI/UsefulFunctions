0c1e3a97170e01ee6843143291f711feac782803
jamesLSI <james.currer@iuk.ukri.org>
2023-08-15 11:41:27 +0100

    Adding Fiscal Year function

---
+++ R/fyFunction.R
@@ -0,0 +1,25 @@
+#' Fiscal Year Function
+#'
+#' @param .data is a dataframe
+#' @param date_field is a date column in the dataframe
+#'
+#' @return an additional column in the dataframe with the correct fiscal year
+#' @export
+#'
+#' @examples
+#' data_With_fy <- data_frame %>% 
+#'   mutate(fy_function(.,date_field = To))
+fy_function <- function(.data, date_field){
+  
+  return(.data %>% 
+           mutate(fy = if_else(month(date_field) > 3,
+                               paste0(year(date_field),
+                                      "/",
+                                      str_sub(year(date_field)+1,
+                                              3,4)),
+                               paste0(year(date_field)-1,
+                                      "/",
+                                      str_sub(year(date_field),
+                                              3,4))))
+  )
+}
