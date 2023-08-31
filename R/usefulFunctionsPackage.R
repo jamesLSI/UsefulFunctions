7e2316dbbccc09f5a703556d70755ea4cf1d6540
jamesLSI <james.currer@iuk.ukri.org>
2023-08-31 10:31:54 +0100

    adding an update function

--- R/usefulFunctionsPackage.R
+++ R/usefulFunctionsPackage.R
@@ -856,4 +856,18 @@ lm_summary_table_function <- function(summary_table){
   
 }
 
+### update useful functions function ####
 
+#' update usefulfunctions
+#'
+#' @return the lastest version of the package installed from github
+#' @export
+#'
+#' @examples
+#' Will overwrite current installed version but will not update out of date dependancies
+#' update_useful_functions()
+update_useful_functions <- function(){
+devtools::install_github("jamesLSI/UsefulFunctions",
+                         force = TRUE,
+                         upgrade = "never")
+}
