4608595162be219c349a9454531b8955e6ec1007
jamesLSI <james.currer@iuk.ukri.org>
2024-05-15 13:33:52 +0100

    new fy function

--- R/fyFunction.R
+++ R/fyFunction.R
@@ -8,18 +8,17 @@
 #'
 #' @examples
 #' data_With_fy <- data_frame %>% 
-#'   mutate(fy_function(.,date_field = To))
-fy_function <- function(.data, date_field){
+#'   mutate(fy_function(., new_col = fy_to, date_field = To))
+fy_function <- function(.data, new_col, date_field){
   
   return(.data %>% 
-           mutate(fy = if_else(month(date_field) > 3,
-                               paste0(year(date_field),
-                                      "/",
-                                      str_sub(year(date_field)+1,
-                                              3,4)),
-                               paste0(year(date_field)-1,
-                                      "/",
-                                      str_sub(year(date_field),
-                                              3,4))))
-  )
+           mutate({{new_col}} := if_else(month(date_field) > 3,
+                                         paste0(year(date_field),
+                                                "/",
+                                                str_sub(year(date_field)+1,
+                                                        3,4)),
+                                         paste0(year(date_field)-1,
+                                                "/",
+                                                str_sub(year(date_field),
+                                                        3,4)))))
 }
